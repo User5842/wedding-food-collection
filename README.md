969a3065a75edb70ac75c88ec78a6a81b7e9c268
User5842 <r.negron1@umiami.edu>
2024-12-16 22:49:44 -0500

    randomly generate header images

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ My (soon-to-be) wife and I needed a simple way to collect responses from our gue
 It's a simple solution that exposed me to technologies I have always wanted to explore further, specifically [zod](https://zod.dev/) and [shadcn/ui](https://ui.shadcn.com/).
 
 I wasn't originally planning on uploading this. Still, because it ended up being a more-than-trivial-sized project, doing so would serve, if anything, as a portfolio item.
+
+## Updates
+
+- [x] Random images for the header
+- [ ] ...
--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -2,30 +2,47 @@
 
 import { Guest } from "../interfaces/Guest.interface";
 import GuestSelection from "./GuestSelection";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Family } from "../interfaces/Family.interface";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import party from "@/assets/party.webp";
+import dachshund from "@/assets/dachshund.webp";
+import partyTwo from "@/assets/party-two.webp";
+import partyThree from "@/assets/party-three.webp";
+import partyFour from "@/assets/party-four.webp";
+import partyFive from "@/assets/party-five.webp";
 import FamilyForm from "./FamilyForm";
 
 interface MainProps {
   families: Family[];
 }
 
+const images = [dachshund, party, partyTwo, partyThree, partyFour, partyFive];
+
 export default function Main({ families }: MainProps) {
   const [family, setFamily] = useState<Family | null>(null);
+  const [image, setImage] = useState<StaticImageData | null>(null);
+
+  useEffect(() => {
+    const image = images[Math.floor(Math.random() * images.length)];
+    setImage(image);
+  }, []);
 
   const onGuestSelection = (guest: Guest) =>
     setFamily(families.find(({ id }) => id === guest.familyId)!);
 
   return (
     <main className="container mx-auto max-w-prose text-center space-y-8 p-4">
-      <Image
-        priority
-        src={party}
-        className="w-full h-full object-cover"
-        alt="Picture of the author"
-      />
+      <div className="w-[546px] h-[546px]">
+        {image && (
+          <Image
+            priority
+            src={image}
+            className="w-full h-full object-cover"
+            alt="Picture of the author"
+          />
+        )}
+      </div>
       <header className="space-y-2">
         <h1 className="text-4xl font-bold">Feasting with Karen and Rafael!</h1>
         <p>
