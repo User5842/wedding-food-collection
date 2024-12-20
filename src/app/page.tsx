e883650a785e4ad1f391bd97180dc2747c64d55d
User5842 <r.negron1@umiami.edu>
2024-12-20 18:27:40 -0500

    finito... for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,22 @@ import Main from "./components/Main";
 import prisma from "./lib/db";
 import { Family } from "./interfaces/Family.interface";
 
+// import families from "@/data/families.json";
+// import guests from "@/data/guests.json";
+
 export default async function Home() {
   // await prisma.family.createMany({ data: families });
   // await prisma.guest.createMany({ data: guests });
 
-  await prisma.guest.deleteMany();
-  await prisma.family.deleteMany();
+  // await prisma.guest.deleteMany();
+  // await prisma.family.deleteMany();
 
-  // const families = (await prisma.family.findMany({
-  //   include: { guests: true },
-  //   where: {
-  //     responseRecorded: false,
-  //   },
-  // })) as Family[];
+  const families = (await prisma.family.findMany({
+    include: { guests: true },
+    where: {
+      responseRecorded: false,
+    },
+  })) as Family[];
 
-  return <Main families={[]} />;
+  return <Main families={families} />;
 }
